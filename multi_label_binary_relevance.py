f99a2ba5a4e20e95b07c128a310dfa36238d0e99
DESKTOP-S6LCBCL\dongj <dongjidan@126.com>
2019-04-15 15:45:38 +0800

    init files

---
+++ multi_label_binary_relevance.py
@@ -0,0 +1,40 @@
+from sklearn import datasets
+from skmultilearn.problem_transform import BinaryRelevance
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+import numpy as np 
+# 使用sklearn生成多标记数据，默认生成100个数据，5个标签，20个特征
+data = datasets.make_multilabel_classification()
+#得到训练数据X，和标签类别Y
+X = data[0]
+Y = data[1]
+logs = []
+#因为做练习嘛，就直接写5，即5个标签
+#得到训练的5个分类器
+for i in range(5):
+	logR = LogisticRegression()
+	logR.fit(X, Y[:, i])
+	logs.append(logR)
+
+#用分类器进行预测
+result = []
+for i in range(5):
+	res = logs[i].predict(X)
+	result.append(res)
+
+#将预测的结果转换成与Y一样的格式
+fres = []
+for i in range(len(result[0])):
+	a = [result[0][i], result[1][i], result[2][i], result[3][i], result[4][i]]
+	fres.append(a)
+
+#精确度
+fres = np.matrix(fres)
+print(accuracy_score(fres, Y))
+
+#使用线程函数的结果，两者一样
+cl = BinaryRelevance(LogisticRegression())
+cl.fit(data[0], data[1])
+pred = cl.predict(data[0])
+acc = accuracy_score(pred, data[1])
+print(acc)
